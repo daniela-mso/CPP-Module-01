82965ea1e75875cd6e70609a03c7b799dd2f0b2a
danielad <danielad@student.42lausanne.ch>
2026-09-23 16:26:01 +0200

    fixed ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,67 +3,135 @@
 #include <fstream>
 
 
-void	replace_str(std::ifstream &file, std::string s1, std::string s2)
+// void	replace_str(std::ifstream &file, std::string s1, std::string s2)
+// {
+// 	if (s1.empty())
+// 	{
+// 		std::cout << std::endl << "Error, cant have an empry search string, bucko!" << std::endl;
+// 		return;
+// 	}
+// 	size_t	s1_length = s1.length();
+
+
+// 	bool	isEmpty = true;
+// 	std::string line;
+// 	while (std::getline(file, line))
+// 	{
+// 		isEmpty = false;
+// 		size_t s1_position = line.find(s1);
+// 		if (s1_position == 0 || !isalnum(line[s1_position - 1]))
+// 		{
+// 			if ( s1_position + s1_length == line.length() ||
+// 				!isalnum(line[s1_position + s1_length]))
+// 			{
+// 				line.erase(s1_position, s1_length);
+// 				line.insert(s1_position, s2);
+// 			}
+// 		}
+
+
+// 		std::cout << line;
+
+
+
+// 	}
+// 	if (isEmpty)
+// 	{
+// 		std::cout << std::endl << "The selected file is empty!" << std::endl;
+// 		return;
+// 	}
+
+
+// }
+
+// int main(int ac, char **av)
+// {
+// 	if (ac != 4)
+// 	{
+// 		std::cout << "Wrong number of arguments!" << std::endl;
+// 		return (1);
+// 	}
+
+// 	std::ifstream myFile(av[1]);
+// 	if (!myFile)
+// 	{
+// 		std::cout << std::endl << "Error, Non existing file selected: " << av[1] << std::endl;
+// 		return (1);
+// 	}
+
+// 	// std::ofstream newFile("newFile.txt");
+// 	// newFile << "hello";
+	
+
+// 	replace_str(myFile, av[2], av[3]);
+
+// 	return 0;
+// }
+
+
+
+
+void	replace_str(std::string filename, std::string s1, std::string s2)
 {
 	if (s1.empty())
 	{
-		std::cout << std::endl << "Error, cant have an empry search string, bucko!" << std::endl;
+		std::cout << std::endl
+			<< "Error, can't have an empty search string, bucko!"
+			<< std::endl;
 		return;
 	}
-	size_t	s1_length = s1.length();
 
+	std::ifstream file(filename.c_str());
+	if (!file)
+	{
+		std::cout << "Error, could not open input file!" << std::endl;
+		return;
+	}
 
-	bool	isEmpty = true;
-	std::string line;
+	std::ofstream newFile((filename + ".replace").c_str());
+	if (!newFile)
+	{
+		std::cout << "Error, could not create output file!" << std::endl;
+		return;
+	}
+
+	size_t		s1_length = s1.length();
+	bool		isEmpty = true;
+	std::string	line;
+
 	while (std::getline(file, line))
 	{
 		isEmpty = false;
-		size_t s1_position = line.find(s1);
-		if (s1_position == 0 || !isalnum(line[s1_position - 1]))
+		size_t s1_position = 0;
+
+		while ((s1_position = line.find(s1, s1_position))
+			!= std::string::npos)
 		{
-			if ( s1_position + s1_length == line.length() ||
-				!isalnum(line[s1_position + s1_length]))
-			{
-				line.erase(s1_position, s1_length);
-				line.insert(s1_position, s2);
-			}
+			line.erase(s1_position, s1_length);
+			line.insert(s1_position, s2);
+			s1_position += s2.length();
 		}
 
+		newFile << line;
 
-		std::cout << line;
-
-
-
+		if (!file.eof())
+			newFile << std::endl;
 	}
+
 	if (isEmpty)
-	{
-		std::cout << std::endl << "The selected file is empty!" << std::endl;
-		return;
-	}
-
-
+		std::cout << "The selected file is empty!" << std::endl;
 }
+
 
 int main(int ac, char **av)
 {
 	if (ac != 4)
 	{
 		std::cout << "Wrong number of arguments!" << std::endl;
-		return (1);
+		return 1;
 	}
 
-	std::ifstream myFile(av[1]);
-	if (!myFile)
-	{
-		std::cout << std::endl << "Error, Non existing file selected: " << av[1] << std::endl;
-		return (1);
-	}
-
-	// std::ofstream newFile("newFile.txt");
-	// newFile << "hello";
-	
-
-	replace_str(myFile, av[2], av[3]);
+	replace_str(av[1], av[2], av[3]);
 
 	return 0;
 }
